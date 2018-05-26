e110ab23c5d1f2e0cb590270e75ff8998781276d
Sean Fong <seancfong@gmail.com>
2018-05-25 21:15:54 -0700

    Update the rules page

--- scripts/rules.js
+++ scripts/rules.js
@@ -19,17 +19,22 @@ function dispRules() {
 
   // GRAYSCALE
   c.font = "80px Tahoma";
-  c.fillStyle = "rgba(256, 256, 256, 0.65)";
+  c.fillStyle = "rgba(256, 256, 256, 0.8)";
 
   c.fillText("The Gist", canvas.width / 2, (canvas.height / 2) - 100);
+  c.font = "30px Arial";
+  c.fillStyle = "rgba(256, 256, 256, 0.55)";
+  c.fillText("You will be presented rectangles and", canvas.width / 2, (canvas.height / 2) - 50);
+  c.fillText("circles both following the same", canvas.width / 2, (canvas.height / 2) - 10);
+  c.fillText("gradient of colors.", canvas.width / 2, (canvas.height / 2) + 30);
+  c.fillText("There will always be 1 less", canvas.width / 2, (canvas.height / 2) + 70);
+  c.fillText("circle than rectangles.", canvas.width / 2, (canvas.height / 2) + 110);
+  c.fillText("Simply click the rectangle with", canvas.width / 2, (canvas.height / 2) + 150);
+  c.fillText("the extra color.", canvas.width / 2, (canvas.height / 2) + 190);
+
+
   c.font = "35px Arial";
   c.fillStyle = "rgba(256, 256, 256, 0.65)";
-  c.fillText("stuff.", canvas.width / 2, (canvas.height / 2) + 70);
-
-
-
-  c.font = "35px Arial";
-  c.fillStyle = "rgba(256, 256, 256, 0.65)";
-  c.fillText("Click to begin.", canvas.width / 2, (canvas.height / 2) + 70);
+  c.fillText("Click to begin.", canvas.width / 2, (canvas.height / 2) + 240);
 
 }
