9b64012a3fd1188c4b6dfd94a20936cb384a87e3
Sean Fong <seancfong@gmail.com>
2018-05-26 17:49:14 -0700

    Tweak gradient generation

--- scripts/gradients/gradients_bottom.js
+++ scripts/gradients/gradients_bottom.js
@@ -1,6 +1,6 @@
 function makeGradient() {
   var ranges = [];
-  var isGray = Math.floor(Math.random() * gradientAmt);
+  var isGray = Math.floor(Math.random() * (gradientAmt * 2));
   var r = Math.floor(Math.random() * 180);
   var g = Math.floor(Math.random() * 180);
   var b = Math.floor(Math.random() * 180);
@@ -9,15 +9,15 @@ function makeGradient() {
   var g_right = Math.floor(Math.random() * (255 - g)) + g;
   var b_right = Math.floor(Math.random() * (255 - b)) + b;
 
-  var r_sep = Math.ceil((r_right - r) / gradientAmt) + 5;
+  var r_sep = Math.ceil((r_right - r) / gradientAmt) + 2;
   var g_sep = Math.ceil((g_right - g) / gradientAmt) + 5;
-  var b_sep = Math.ceil((b_right - b) / gradientAmt) + 5;
+  var b_sep = Math.ceil((b_right - b) / gradientAmt) + 7;
 
-  if (isGray > 3) {
+  if (isGray == 1) {
     g = r;
     b = r;
-    g_sep = r_sep;
-    b_sep = r_sep;
+    g_sep = r_sep + 4;
+    b_sep = r_sep + 10;
   }
   for (var i = 0; i < gradientAmt; i++) {
     ranges.push([(r + (r_sep * i)) % 255,
